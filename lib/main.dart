d2f6314601312644537fd654329eaedaba3f0e7a
Ritutosh-Bhatia <101282130+Ritutosh-Bhatia@users.noreply.github.com>
2022-03-22 20:27:44 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:log_in/ui/homepage.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
